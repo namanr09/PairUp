05827797048f7bc040a8df97e551f2b95dc13d68
naman <rohitasnaman09@gmail.com>
2025-07-18 17:31:58 +0530

    Reinitialization

--- main-app/app/src/main/java/com/chat/pairup/visual/splashscreen/SplashScreen.kt
+++ main-app/app/src/main/java/com/chat/pairup/visual/splashscreen/SplashScreen.kt
@@ -1,2 +1,10 @@
 package com.chat.pairup.visual.splashscreen
+// splash screen code
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
 
+
+@Composable
+fun SplashScreen(){
+    Box(modifier = Modifier.fillMaxSize())
+}
